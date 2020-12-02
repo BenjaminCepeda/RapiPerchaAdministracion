351d3b2d0aedcf95ef3d0742b2348bd5cc540bca
Benjamin Cepeda <bcepeda_98@yahoo.com>
2020-12-02 18:00:53 -0500

    Creación de estructura de proyecto

---
+++ src/ec/com/kodice/rapipercha/administracion/presentacion/Principal.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ec.com.kodice.rapipercha.administracion.presentacion;
+
+/**
+ * Esta clase contiene los atributos y metodos de la clase Principal
+ * @author Benjamin Cepeda
+ * @version v1.0
+ * @date 2020/12/02 
+ */
+public class Principal {
+
+    /**
+     * Metodo principal de la clase
+     * @param args Argumento de linea de comando
+     */
+    public static void main(String[] args) {
+        //TODO 
+        FrmLogin frmLogin = new FrmLogin();
+        frmLogin.setVisible(true);
+   }
+}
